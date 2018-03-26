ec7e59e1b5f900408ee63b5233f276d709f05b64
sulian <sulian@sulians-air-2.fios-router.home>
2018-03-26 13:11:57 -0700

    move分左右方法，可以自由移动

--- SpaceInvader/src/SpaceInvader.java
+++ SpaceInvader/src/SpaceInvader.java
@@ -1,17 +1,31 @@
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class SpaceInvader extends JPanel implements Runnable {
+public class SpaceInvader extends JPanel implements Runnable, KeyListener {
 	private static final long serialVersionUID = 463975730322821834L;
-	int x = 0;
+	int width = 550;
+	int height = 550;
+	int playX = 0;
+	int playY = (int) (height * 0.9);
+	int ufoX = 0;
+	int ufoY = (int) (height * 0.2);
+	int ufoSpeed = 1;
+	int playSpeed = 2;
+	int speed = 0;
+	boolean movingLeft = false, movingRight = false, newFire = false;
 
 	public SpaceInvader() {
 		JFrame jf = new JFrame();
-		jf.setSize(555, 555);
+		jf.setSize(width, height);
+		jf.setLocationRelativeTo(null);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.add(this);
 		jf.setVisible(true);
+		jf.addKeyListener(this);
 		new Thread(this).start();
 	}
 
@@ -20,10 +34,10 @@ public class SpaceInvader extends JPanel implements Runnable {
 		super.paintComponent(g);
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 5; j++) {
-				g.fillOval(x + i * 30, 100 + j * 30, 20, 20);
+				g.fillOval(ufoX + i * 30, ufoY + j * 30, 20, 20);
 			}
 		}
-		g.fillOval(x + 40, 500, 30, 30);
+		g.fillOval(playX + 40, playY, 30, 30);
 		g.dispose();
 	}
 
@@ -34,7 +48,13 @@ public class SpaceInvader extends JPanel implements Runnable {
 	@Override
 	public void run() {
 		while (true) {
-			x++;
+			ufoStart();
+			// 要修改的地方
+			moveLeft(movingLeft);
+			moveRight(movingRight);
+			if (newFire) {
+				// 子弹
+			}
 			repaint();
 
 			try {
@@ -42,6 +62,73 @@ public class SpaceInvader extends JPanel implements Runnable {
 			} catch (InterruptedException e) {
 			}
 		}
+	}
+
+	public void ufoStart() {
+		ufoX += ufoSpeed;
+		if (ufoX == width) {
+			ufoX = 0;
+		}
+	}
+
+	public void moveLeft(boolean movingLeft) {
+		if (movingLeft) {
+			speed = playSpeed;
+		} else {
+			speed = 0;
+		}
+		playX -= speed;
+		if (playX < 0) {
+			playX = 0;
+		}
+	}
+
+	public void moveRight(boolean movingRight) {
+		if (movingRight) {
+			speed = playSpeed;
+		} else {
+			speed = 0;
+		}
+		playX += speed;
+		if (playX > width) {
+			playX = width;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_LEFT) {
+			movingLeft = true;
+		}
+		if (key == KeyEvent.VK_RIGHT) {
+			movingRight = true;
+		}
+		if (key == KeyEvent.VK_CONTROL) {
+			newFire = true;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_LEFT) {
+			movingLeft = false;
+		}
+		if (key == KeyEvent.VK_RIGHT) {
+			movingRight = false;
+		}
+		if (key == KeyEvent.VK_CONTROL) {
+			newFire = false;
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 }
 
