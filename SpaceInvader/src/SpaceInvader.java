2bbccd569ada3ffb9b6fbcb4f7e4f50e365cbe64
sulian <sulian@sulians-air-2.fios-router.home>
2018-03-28 13:51:58 -0700

    player能正常射出子弹，并且消掉UFO

--- SpaceInvader/src/SpaceInvader.java
+++ SpaceInvader/src/SpaceInvader.java
@@ -4,6 +4,7 @@ import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -27,19 +28,22 @@ public class SpaceInvader extends JPanel implements Runnable, KeyListener {
 	// player移动速度
 	int playSpeed = 2;
 	// 子弹速度
-	int bulletSpeed = 4;
+	int bulletSpeed = 8;
 	int speed = 0;
 	//子弹圆心位置
-	public static int bulletRadius = 5;
+	public static int bulletRadius = 2;
 	public int bulletY = 0;//playY;
-	public int bulletX = 0;//playX + playDiam / 2;
-
+ 	public int bulletX = 0;//playX + playDiam / 2;
+ 	
 	// 键盘输入判断
 	boolean movingLeft = false, movingRight = false;
 	boolean fireLive = false;// 判断是否出界，或者消失
+	//boolean processedFire = false;//判断子弹是否被处理过（出界或消失）
 	boolean newFire = false; // 判断是否装新子弹
+	//boolean collided = false;
 
 	List<Point> fireList = new ArrayList<Point>();//子弹夹
+	List<Point> ufoList = new ArrayList<Point>();//入侵者集
 
 
 	public SpaceInvader() {
@@ -57,19 +61,26 @@ public class SpaceInvader extends JPanel implements Runnable, KeyListener {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		
-		for (int i = 0; i < 10; i++) {
+		/*for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 5; j++) {
 				g.fillOval(ufoX - ufoRadius + i * 30, ufoY - ufoRadius + j * 30, ufoRadius*2, ufoRadius*2);
 			}
+		}*/
+		
+		for(int i = 0; i < ufoList.size(); i++) {
+			Point p = ufoList.get(i);
+			g.fillOval(p.x - ufoRadius, p.y - ufoRadius, ufoRadius*2, ufoRadius*2);
 		}
 		
 		g.fillOval(playX - playRadius, playY- playRadius, playRadius*2, playRadius*2);
-		
+		//if(hasFire) {
+		//int a = fireList.size();
 		for (int i = 0; i < fireList.size(); i++) {
 			Point p = fireList.get(i);
 			g.fillOval(p.x - bulletRadius, p.y - bulletRadius, bulletRadius *2, bulletRadius *2);
 		}
-		g.drawString("Bullets Count:" + fireList.size(), 10, 50);
+		//}
+//		g.drawString("Bullets Count:" + fireList.size(), 10, 50);
 		g.dispose();
 	}
 
@@ -80,35 +91,89 @@ public class SpaceInvader extends JPanel implements Runnable, KeyListener {
 	// 当前状态
 	@Override
 	public void run() {
+		ufoShow();
 		while (true) {
 			ufoStart();
 			// 要修改的地方
 			move();
 			// 子弹
-			if(!fireList.isEmpty()) {
-				for (int i = 0; i < fireList.size(); i++) {
-					Point fire = fireList.get(i);
-					fire.y -= bulletSpeed;
-					if(fire.y < 0 ) {
-						fireList.remove(i);
+			Iterator<Point> itr = fireList.iterator();
+			while(itr.hasNext()) {
+				Point fire = itr.next();
+				fire.y -= bulletSpeed;
+				if(fire.y < 0) {
+					itr.remove();
+				}
+				
+				Iterator<Point> itrUFO = ufoList.iterator();
+				while(itrUFO.hasNext()) {
+					Point ufo = itrUFO.next();
+					if(ufo.y >=  fire.y - ufoRadius - bulletRadius && ufo.y <=  fire.y + ufoRadius + bulletRadius 
+							&& ufo.x <= fire.x + ufoRadius + bulletRadius && ufo.x >= fire.x - ufoRadius - bulletRadius) {
+							itr.remove();
+							itrUFO.remove();
 					}
 				}
+
 			}
 
-			repaint();
+			/*if (!fireList.isEmpty()) {
+				for (int i = 0; i < fireList.size();) {
+					Point fire = fireList.get(i);
+					fire.y -= bulletSpeed;
+					if (fire.y < 0) {
+						fireList.remove(i);	
+					}
+					else {
+						i++;
+					}*/
+					
+					/*if(!ufoList.isEmpty()) {
+						for(int j = 0; j < ufoList.size();) {
+							Point temp = ufoList.get(j);
+							if(temp.y >=  fire.y - ufoRadius - bulletRadius && temp.y <=  fire.y + ufoRadius + bulletRadius 
+									&& temp.x <= fire.x + ufoRadius + bulletRadius && temp.x >= fire.x - ufoRadius - bulletRadius) {
+									ufoList.remove(j);
+									fireList.remove(i);
+								} else {
+									j++;
+									i++;
+								}
+							}
+						}
+						}*/
 
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
+				repaint();
+
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+				}
+			}
+}
+	
+	public void ufoShow() {
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 5; j++) {
+				int ufoPosX = ufoX - ufoRadius + i * 30;
+				int ufoPosY = ufoY - ufoRadius + j * 30;
+				Point ufoPos = new Point(ufoPosX, ufoPosY);
+				ufoList.add(ufoPos);
 			}
 		}
 	}
 
 	// ufo运动控制
 	public void ufoStart() {
-		ufoX += ufoSpeed;
-		if (ufoX == width) {
-			ufoX = 0;
+		//ufoX += ufoSpeed;
+		//ufoShow();
+		for(int i = 0; i < ufoList.size(); i++) {
+			Point ufoTemp = ufoList.get(i);
+			ufoTemp.x += ufoSpeed;
+		
+			if (ufoTemp.x == width) {
+				ufoTemp.x = 0;
+			}
 		}
 	}
 
@@ -129,15 +194,6 @@ public class SpaceInvader extends JPanel implements Runnable, KeyListener {
 			playX = width - playRadius;
 		}
 	}
-	// new子弹
-	/*public Bullet fire(boolean newFire) {
-		bulletX = playX + playDiam / 2;
-		bulletY = playY;
-		Bullet nf = new Bullet(bulletX, bulletY, this);
-		fireList.add(nf);
-		return nf;
-
-	}*/
 
 	@Override
 	public void keyPressed(KeyEvent e) {
